2028aff15343bf8552d534cf0f967894e208ab84
tarielTitirashvili <tarieltitirashvili@gmail.com>
2024-11-08 00:19:31 +0400

    checkboxes

--- components/shared/filters/index.tsx
+++ components/shared/filters/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Title } from '../title'
 import { useTranslations } from 'next-intl'
+import { FilterCheckbox } from './checkbox'
 
 const Filters = () => {
   const t = useTranslations()
   return (
     <div>
-      <Title text={t('sidebarFilters.title')} />
+      <Title
+        text={t('sidebarFilters.title')}
+        size="sm"
+        className="mb-5 font-bold"
+      />
+      <div className='flex flex-col gap-2'>
+        <FilterCheckbox text={t('sidebarFilters.editable')} value='0'/>
+        <FilterCheckbox text={t('sidebarFilters.new')} value='0'/>
+      </div>
     </div>
   )
 }
