e528a6b562e1283b8a6fdcfc232acdf7eafef8e2
tarielTitirashvili <tarieltitirashvili@gmail.com>
2024-11-08 18:50:14 +0400

    range slider and inputs

--- components/shared/filters/index.tsx
+++ components/shared/filters/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Title } from '../title'
 import { useTranslations } from 'next-intl'
 import { FilterCheckbox } from './checkbox'
+import { Input } from '@/components/ui'
+import { RangeSlider } from '../range-slider'
 
 export const Filters = () => {
   const t = useTranslations()
@@ -15,8 +17,19 @@ export const Filters = () => {
       <div className='flex flex-col gap-2'>
         <FilterCheckbox text={t('sidebarFilters.editable')} value='0'/>
         <FilterCheckbox text={t('sidebarFilters.new')} value='1'/>
-        
       </div>
+      <div className='mt-5 flex flex-col border-y border-y-neutral-100 py-6 pb-7'>
+        <Title
+          text={t('sidebarFilters.price')}
+          size="sm"
+          className="mb-5 font-bold"
+        />
+        <div className='flex gap-3'>
+          <Input type='number' placeholder='0' min={0} max={3000} defaultValue={0} />
+          <Input type='number' placeholder='30000' min={1} max={3000} defaultValue={3000} />
+        </div>
+      </div>
+        <RangeSlider min={0} max={3000} step={10} value={[0, 3000]} />
     </div>
   )
 }
